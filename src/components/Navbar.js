885de24a5a692e069ff024a6f1b5061dfb561103
Charlyeneh <charleschukwunweikeeneh@gmail.com>
2023-02-13 19:14:11 +0100

    Add the calculator components

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => (
+	<nav className="nav">
+		<h1>Grape Law Firm Job Assessment - A Simple Calculator Project</h1>
+	</nav>
+);
+
+export default Navbar;
